c8bbc824babe00476eb383ed651747402846e9ff
russellxz <162611099+russellxz@users.noreply.github.com>
2025-12-15 09:14:52 -0500

    Update Play2.js

--- plugins/pluginsdescargas/Play2.js
+++ plugins/pluginsdescargas/Play2.js
@@ -13,7 +13,7 @@ const { pipeline } = require("stream");
 const streamPipe = promisify(pipeline);
 
 // ==== CONFIG DE TU API ====
-const API_BASE = (process.env.API_BASE || "https://api-sky-test.ultraplus.click").replace(/\/+$/, "");
+const API_BASE = (process.env.API_BASE || "https://api-sky.ultraplus.click").replace(/\/+$/, "");
 const API_KEY = process.env.API_KEY || "Russellxz";
 
 // Defaults
