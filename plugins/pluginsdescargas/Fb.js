24af01f868892d92fdc879bab830e2ef96a444a7
russellxz <162611099+russellxz@users.noreply.github.com>
2025-12-15 09:15:25 -0500

    Update Fb.js

--- plugins/pluginsdescargas/Fb.js
+++ plugins/pluginsdescargas/Fb.js
@@ -6,7 +6,7 @@ const fs = require("fs");
 const path = require("path");
 
 // === Config API nueva ===
-const API_BASE = (process.env.API_BASE || "https://api-sky-test.ultraplus.click").replace(/\/+$/, "");
+const API_BASE = (process.env.API_BASE || "https://api-sky.ultraplus.click").replace(/\/+$/, "");
 const API_KEY  = process.env.API_KEY  || process.env.SKY_API_KEY || global.SKY_API_KEY || "Russellxz";
 
 // Opcional si tu API lo soporta (docs)
